f146c6a996fffed882fa25b7ee83de407a2f6dd5
rguo123 <richardg7890@gmail.com>
2019-09-14 20:38:55 -0400

    reorganizing repo structure

---
+++ multiview/__init__.py
@@ -0,0 +1,9 @@
+"""
+Multiview Learning module for Python
+==================================
+multiview is a Python module for various multiview learning functionalities.
+
+TODO: add better summary once MVP is determined
+"""
+
+# Put top level functionalities in here
