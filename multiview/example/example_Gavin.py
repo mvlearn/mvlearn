84a31ca9f9abcfbe682c49fcceec81ee3b41a288
Gavin Mischler <gavinmischler@gmail.com>
2019-09-27 15:02:05 -0400

    added example_Gavin.py for PR

---
+++ multiview/example/example_Gavin.py
@@ -0,0 +1,25 @@
+"""
+example_Gavin.py
+====================================
+Example of how to contribute code to multiview.
+"""
+
+
+def example_function_gavin(param = "defaultval"):
+    """
+    Prints the input and returns True.
+    Good link on how to write these napolean docs: 
+    https://sphinxcontrib-napoleon.readthedocs.io/en/latest/
+
+    Parameters
+    ----------
+    param
+        A string to be printed.
+
+    Returns
+    -------
+    string
+        Whatever param is set to.
+    """
+    print(param)
+    return True
