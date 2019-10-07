98b1cc499060491687c4bab081b45a06bb3e4bcf
rguo123 <richardg7890@gmail.com>
2019-10-07 09:59:01 -0400

    rsm script and tests

--- multiview/construct/rsm.py
+++ multiview/construct/rsm.py
@@ -7,12 +7,13 @@ Random subspace method for view construction.
 import numpy as np
 import random
 
+
 def random_subspace_method(X, n_features=None, n_views=1):
     """
     Random Subspace Method for constructing multiple views.
-    Each view is constructed by randomly selecting n_features 
+    Each view is constructed by randomly selecting n_features
     (columns) from X. All unselected features are set to 0.
-    
+
     Original paper: https://ieeexplore.ieee.org/document/709601
 
     Parameters
@@ -23,9 +24,11 @@ def random_subspace_method(X, n_features=None, n_views=1):
     n_features : int, float
         Number of features to randomly select.
 
-        - If int, then consider n_features as number of columns to select.
+        - If int, then consider n_features as number of columns
+        to select.
 
-        - If float, then consider n_features*n_cols as number of columns to select.
+        - If float, then consider n_features*n_cols as number of columns
+        to select.
 
     n_views : int, float optional (default = 1)
         Number of views to construct.
@@ -36,7 +39,7 @@ def random_subspace_method(X, n_features=None, n_views=1):
         List of constructed views (each matrix has shape [n_rows, n_cols]).
 
     """
-     
+
     # todo let max_features be float for probability
     _, cols = X.shape
 
@@ -46,25 +49,26 @@ def random_subspace_method(X, n_features=None, n_views=1):
     # check if n_feaures is between 0 and 1
     if n_features < 1:
         n_features = int(n_features*cols)
-        
+
     views = []
-    
+
     for _ in range(n_views):
         view = np.copy(X)
         features_selected = []
-        
+
         while len(features_selected) != n_features:
-            
+
             feature = random.randint(0, cols - 1)
-            
+
             if feature not in features_selected:
                 features_selected.append(feature)
-        
+
+        # set unselected features to zero
         for feature in range(cols):
             if feature not in features_selected:
                 view[:, feature] = 0
         views.append(view)
-    
+
     return views
 
 
@@ -104,7 +108,7 @@ def check_n_features(n_features, cols):
     ----------
     n_features: float, int
         Number of features to randomly select.
-    
+
     cols: int
         Number of features in data matrix.
     """
@@ -119,6 +123,3 @@ def check_n_features(n_features, cols):
         raise Exception("n_features must be less than columns in X.")
 
     return
-
-
-
