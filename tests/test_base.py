acf856633dea97940fa8180057c54766f12a9fbe
rflperry <rflperry@gmail.com>
2019-10-18 14:16:59 -0400

    removed pairwise

--- tests/test_base.py
+++ tests/test_base.py
@@ -7,7 +7,6 @@ from numpy.testing import assert_equal
 def test_base_embed():
     base = BaseEmbed()
 
-    assert_equal(base._pairwise())
     base.fit(Xs=None, y=None)
     base.transform(Xs=None)
     base.fit_transform(Xs=None, y=None)
