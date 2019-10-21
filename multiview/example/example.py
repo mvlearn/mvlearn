b1149f9ec61fb33fc9e4d31e0b70a604b81fea10
rflperry <rflperry@gmail.com>
2019-10-21 14:52:19 -0400

    added examples

---
+++ multiview/example/example.py
@@ -0,0 +1,25 @@
+"""
+example.py
+====================================
+Example of how to contribute code to multiview.
+"""
+
+
+def example_function(param = "param"):
+    """
+    Prints hello world and returns param.
+    Good link on how to write these napolean docs: 
+    https://sphinxcontrib-napoleon.readthedocs.io/en/latest/
+
+    Parameters
+    ----------
+    param
+        A string to be prtined. Very creative.
+
+    Returns
+    -------
+    string
+        Whatever param is set to.
+    """
+    print("Hello world")
+    return param
