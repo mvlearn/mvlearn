5f7df1fec2ef9d0c917ceecac76029b634060859
theohj826 <theohj826@gmail.com>
2019-10-21 10:14:41 -0400

    edited kcca according to contributing guidelines

--- Pyrcca/rcca.py
+++ Pyrcca/rcca.py
@@ -1,10 +1,8 @@
 """Python module for regularized kernel canonical correlation analysis"""
 
 import h5py
-import joblib
 import numpy as np
 from scipy.linalg import eigh
-
 
 class _CCABase(object):
     def __init__(self, numCV=None, reg=None, regs=None, numCC=None,
@@ -26,45 +24,43 @@ class _CCABase(object):
         self.verbose = verbose
 
     def train(self, data):
-        nT = data[0].shape[0]
         if self.verbose:
             print('Training CCA, kernel = %s, regularization = %0.4f, '
                   '%d components' % (self.ktype, self.reg, self.numCC))
 
-        comps = kcca(data, self.reg, self.numCC, kernelcca=self.kernelcca,
+        comps_ = kcca(data, self.reg, self.numCC, kernelcca=self.kernelcca,
                      ktype=self.ktype, gausigma=self.gausigma,
                      degree=self.degree)
-        self.cancorrs, self.ws, self.comps = recon(data, comps,
+        self.cancorrs_, self.ws_, self.comps_ = recon(data, comps_,
                                                    kernelcca=self.kernelcca)
         if len(data) == 2:
-            self.cancorrs = self.cancorrs[np.nonzero(self.cancorrs)]
+            self.cancorrs_ = self.cancorrs_[np.nonzero(self.cancorrs_)]
         return self
 
     def validate(self, vdata):
         vdata = [np.nan_to_num(_zscore(d)) for d in vdata]
-        if not hasattr(self, 'ws'):
+        if not hasattr(self, 'ws_'):
             raise NameError('Algorithm has not been trained.')
-        self.preds, self.corrs = predict(vdata, self.ws, self.cutoff)
-        return self.corrs
+        self.preds_, self.corrs_ = predict(vdata, self.ws_, self.cutoff)
+        return self.corrs_
 
     def compute_ev(self, vdata):
         nD = len(vdata)
-        nT = vdata[0].shape[0]
-        nC = self.ws[0].shape[1]
+        nC = self.ws_[0].shape[1]
         nF = [d.shape[1] for d in vdata]
-        self.ev = [np.zeros((nC, f)) for f in nF]
+        self.ev_ = [np.zeros((nC, f)) for f in nF]
         for cc in range(nC):
             ccs = cc+1
             if self.verbose:
                 print('Computing explained variance for component #%d' % ccs)
-            preds, corrs = predict(vdata, [w[:, ccs-1:ccs] for w in self.ws],
+            preds_, corrs_ = predict(vdata, [w[:, ccs-1:ccs] for w in self.ws_],
                                    self.cutoff)
-            resids = [abs(d[0]-d[1]) for d in zip(vdata, preds)]
+            resids = [abs(d[0]-d[1]) for d in zip(vdata, preds_)]
             for s in range(nD):
-                ev = abs(vdata[s].var(0) - resids[s].var(0))/vdata[s].var(0)
-                ev[np.isnan(ev)] = 0.
-                self.ev[s][cc] = ev
-        return self.ev
+                ev_ = abs(vdata[s].var(0) - resids[s].var(0))/vdata[s].var(0)
+                ev_[np.isnan(ev_)] = 0.
+                self.ev_[s][cc] = ev_
+        return self.ev_
 
     def save(self, filename):
         h5 = h5py.File(filename, 'a')
@@ -104,13 +100,13 @@ class CCA(_CCABase):
                         Value can be 'linear' (default) or 'gaussian'.
         verbose (bool): default is True.
     Returns:
-        ws (list): canonical weights
-        comps (list): canonical components
-        cancorrs (list): correlations of the canonical components
+        ws_ (list): canonical weights
+        comps_ (list): canonical components
+        cancorrs_ (list): correlations of the canonical components
                          on the training dataset
-        corrs (list): correlations on the validation dataset
-        preds (list): predictions on the validation dataset
-        ev (list): explained variance for each canonical dimension
+        corrs_ (list): correlations on the validation dataset
+        preds_ (list): predictions on the validation dataset
+        ev_ (list): explained variance for each canonical dimension
     """
     def __init__(self, reg=0., numCC=10, kernelcca=True, ktype=None,
                  verbose=True, cutoff=1e-15):
@@ -121,14 +117,14 @@ class CCA(_CCABase):
         return super(CCA, self).train(data)
 
 
-def predict(vdata, ws, cutoff=1e-15):
+def predict(vdata, ws_, cutoff=1e-15):
     """Get predictions for each dataset based on the other datasets
     and weights. Find correlations with actual dataset."""
-    iws = [np.linalg.pinv(w.T, rcond=cutoff) for w in ws]
-    ccomp = _listdot([d.T for d in vdata], ws)
+    iws = [np.linalg.pinv(w.T, rcond=cutoff) for w in ws_]
+    ccomp = _listdot([d.T for d in vdata], ws_)
     ccomp = np.array(ccomp)
-    preds = []
-    corrs = []
+    preds_ = []
+    corrs_ = []
 
     for dnum in range(len(vdata)):
         idx = np.ones((len(vdata),))
@@ -136,10 +132,10 @@ def predict(vdata, ws, cutoff=1e-15):
         proj = ccomp[idx > 0].mean(0)
         pred = np.dot(iws[dnum], proj.T).T
         pred = np.nan_to_num(_zscore(pred))
-        preds.append(pred)
+        preds_.append(pred)
         cs = np.nan_to_num(_rowcorr(vdata[dnum].T, pred.T))
-        corrs.append(cs)
-    return preds, corrs
+        corrs_.append(cs)
+    return preds_, corrs_
 
 
 def kcca(data, reg=0., numCC=None, kernelcca=True, ktype='linear',
@@ -189,18 +185,17 @@ def kcca(data, reg=0., numCC=None, kernelcca=True, ktype='linear',
 
 
 def recon(data, comp, corronly=False, kernelcca=True):
-    nT = data[0].shape[0]
     # Get canonical variates and CCs
     if kernelcca:
-        ws = _listdot(data, comp)
+        ws_ = _listdot(data, comp)
     else:
-        ws = comp
-    ccomp = _listdot([d.T for d in data], ws)
-    corrs = _listcorr(ccomp)
+        ws_ = comp
+    ccomp = _listdot([d.T for d in data], ws_)
+    corrs_ = _listcorr(ccomp)
     if corronly:
-        return corrs
+        return corrs_
     else:
-        return corrs, ws, ccomp
+        return corrs_, ws_, ccomp
 
 def _zscore(d): return (d - d.mean(0)) / d.std(0)
 
@@ -214,13 +209,13 @@ def _listdot(d1, d2): return [np.dot(x[0].T, x[1]) for x in zip(d1, d2)]
 def _listcorr(a):
     """Returns pairwise row correlations for all items in array as a list of matrices
     """
-    corrs = np.zeros((a[0].shape[1], len(a), len(a)))
+    corrs_ = np.zeros((a[0].shape[1], len(a), len(a)))
     for i in range(len(a)):
         for j in range(len(a)):
             if j > i:
-                corrs[:, i, j] = [np.nan_to_num(np.corrcoef(ai, aj)[0, 1])
+                corrs_[:, i, j] = [np.nan_to_num(np.corrcoef(ai, aj)[0, 1])
                                   for (ai, aj) in zip(a[i].T, a[j].T)]
-    return corrs
+    return corrs_
 
 
 def _rowcorr(a, b):
