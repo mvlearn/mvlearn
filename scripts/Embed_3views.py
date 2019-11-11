c3f5bdbd61beff7ebea8786c64a170bf131dcb61
rflperry <rflperry@gmail.com>
2019-11-11 10:14:54 -0500

    added scripts

---
+++ scripts/Embed_3views.py
@@ -0,0 +1,9 @@
+from multiview.embed import gcca
+import numpy as np
+from pathlib import Path
+import pandas as pd
+import dataloader
+
+bblids,fmri_files,sc_files,goa_matched = dataloader.get_files()
+
+
