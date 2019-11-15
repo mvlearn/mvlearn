568d410230b02b537136c78e05d689ee42749264
Alexander Chang <alexc3071@gmail.com>
2019-11-15 05:00:11 -0500

    Added tutorial file

--- multiview/cluster/mv_k_means.py
+++ multiview/cluster/mv_k_means.py
@@ -183,7 +183,7 @@ class MultiviewKMeans(BaseCluster):
 
             # Compute expectation
             distances = self._compute_distance(Xs[view], self._centroids[view])
-            new_partitions = np.argmin(distances, axis=0).flatten()
+            partitions[view] = np.argmin(distances, axis=0).flatten()
 
             # Recompute the objective function
             o_funct = 0
@@ -209,7 +209,7 @@ class MultiviewKMeans(BaseCluster):
             v1_partitions = np.argmin(v1_distances, axis=0).flatten()
             v2_distances = self._compute_distance(Xs[1], self._centroids[1])
             v2_partitions = np.argmin(v2_distances, axis=0).flatten()
-
+            
             # Find data points in the same partition in both views
             part_indices = (v1_partitions == clust) * (v2_partitions == clust)
 
