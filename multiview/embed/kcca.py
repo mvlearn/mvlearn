ae02b9c9c4fc46a2223e46732539a62ddda1900a
theohj826 <theohj826@gmail.com>
2019-12-20 18:31:26 -0500

    moved kcca description

--- multiview/embed/kcca.py
+++ multiview/embed/kcca.py
@@ -3,10 +3,32 @@ kcca.py
 ====================================
 Python module for kernel canonical correlation analysis (kCCA)
 
+CCA aims to find useful projections of the
+high- dimensional variable sets onto the compact
+linear representations, the canonical components (components_).
+
+Each resulting canonical variate is computed from
+the weighted sum of every original variable indicated
+by the canonical weights (weights_).
+
+The canonical correlation (cancorrs_) quantifies the linear
+correspondence between the two views of data
+based on Pearson’s correlation between their
+canonical components.
+
+Canonical correlation can be seen as a metric of
+successful joint information reduction between two views
+and, therefore, routinely serves as a performance measure for CCA.
+
+The kernel generalization of CCA, kernel CCA, is used when
+there are nonlinear relations between two views.
+
+
 Code modified from UC Berkeley, Gallant lab
 (https://github.com/gallantlab/pyrcca)
 Copyright 2016, UC Berkeley, Gallant lab.
 """
+
 from .base import BaseEmbed
 from ..utils.utils import check_Xs
 
@@ -73,26 +95,9 @@ class KCCA(BaseEmbed):
 
     def fit(self, Xs):
         """
-        CCA aims to find useful projections of the
-        high- dimensional variable sets onto the compact
-        linear representations, the canonical components (components_).
-
-        Each resulting canonical variate is computed from
-        the weighted sum of every original variable indicated
-        by the canonical weights (weights_).
-
-        The canonical correlation (cancorrs_) quantifies the linear
-        correspondence between the two views of data
-        based on Pearson’s correlation between their
-        canonical components.
-
-        Canonical correlation can be seen as a metric of
-        successful joint information reduction between two views
-        and, therefore, routinely serves as a performance measure for CCA.
-
-        The kernel generalization of CCA, kernel CCA, is used when
-        there are nonlinear relations between two views.
-
+        Creates kcca mapping by determining
+        canonical weghts from Xs.
+        
         Parameters
         ----------
         Xs : list of array-likes or numpy.ndarray
