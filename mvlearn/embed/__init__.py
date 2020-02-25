21ea94c154b99bcebc2f148a0f19da233f00a61a
Gavin <gavinmischler@gmail.com>
2020-02-24 21:24:02 -0500

    updated dcca.py and __init__.py to include it

--- mvlearn/embed/__init__.py
+++ mvlearn/embed/__init__.py
@@ -4,6 +4,7 @@ from .pls import partial_least_squares_embedding
 from .mvmds import MVMDS
 from .splitae import SplitAE
 from .kcca import KCCA
+from .dcca import DCCA
 
 __all__ = ["GCCA", "Omnibus", "partial_least_squares_embedding",
-           "MVMDS", "SplitAE", "KCCA"]
+           "MVMDS", "SplitAE", "KCCA", "DCCA"]
