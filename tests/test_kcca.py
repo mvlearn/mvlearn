579a09f8340ba0030d2adc5ae390e4c2ebe7f4d5
theohj826 <theohj826@gmail.com>
2020-03-04 11:45:23 -0500

    first finalization of kcca

--- tests/test_kcca.py
+++ tests/test_kcca.py
@@ -1,6 +1,6 @@
 # KCCA Unit Tests
 
-from multiview.embed.kcca import KCCA, _zscore, _rowcorr, _listdot, _listcorr, _make_kernel
+from multiview.embed.kcca import KCCA, center_norm, make_kernel
 import numpy as np
 import pytest
 
@@ -36,9 +36,9 @@ kcca_ft = kcca_l.fit_transform([train1, train2])
 kcca_f = kcca_l.fit([train1, train2])
 kcca_t = kcca_l.transform([train1, train2])
 
-# Test that cancorrs_ is equal to n_components
-def test_numCC_cancorrs_():
-    assert len(kcca_ft.cancorrs_) == n_components
+# Test that number of components is equal to n_components
+def test_numCC_components_():
+    assert len(kcca_ft.components_[0]) and len(kcca_ft.components_[1]) == n_components
 
 # Test that number of views is equal to number of ws_
 def test_numCC_ws_():
@@ -48,14 +48,9 @@ def test_numCC_ws_():
 def test_numCC_comps_():
     assert len(kcca_ft.components_) == 2
     
-#Test that validate() runs
-def test_validate():
-    accuracy = kcca_ft.validate([test1, test2])
-    assert (len(accuracy[0]) == 4 and len(accuracy[1]) == 5)
-    
 # Test that weights from fit equals fit.transform weights
 def test_ktype_weights():
-    assert kcca_t.weights_ == kcca_f
+    assert kcca_t.weights_ == kcca_f.weights_
     
 # Test that components from transform equals fit.transform weights
 def test_ktype_components():
@@ -69,7 +64,7 @@ def test_ktype_gaussian():
     
 # Test that polynomial kernel runs
 def test_ktype_polynomial():
-    kpoly = KCCA(ktype = 'poly', reg = 0.0001, n_components = 2, degree=3)
+    kpoly = KCCA(ktype = 'poly', reg = 0.0001, n_components = 2, degree=3.0)
     kpoly.fit_transform([train1, train2])
     assert len(kpoly.components_) == 2
 
@@ -77,38 +72,20 @@ def test_ktype_polynomial():
 np.random.seed(30)
 a = np.random.uniform(0,1,(10,10))
 b = np.random.uniform(0,1,(10,10))
-c = np.ones((3,3))
+c = np.ones((10,3))
 c[1] = 0
     
-# Test that _rowcorr works
-def test_rowcorr():
-    cs = _rowcorr(a,b)
-    assert len(cs) == 10
+# Test that center_nrom works
+def test_center_norm():
+    b = center_norm(a)
+    assert np.allclose(np.mean(b),0)
     
-# Test that _zscore works
-def test_zscore(): 
-    new = _zscore(c)
-    assert (new[0] == new[2]).all()
-
-# Test listdot works
-def test_listdot(): 
-    x = np.ones((10,10))
-    y = 3*np.ones((10,10))
-    ld = _listdot(x,y)
-    assert ld == [30.0]*10
-    
-# Test _listcorr works
-def test_listcorr():
-    d = [a,b,a,b]
-    lc = _listcorr(d)
-    assert len(lc) == 10
-
 # Test make_kernel
 def test_make_kernel(): 
-    lkernel = _make_kernel(c, normalize=True, ktype="linear", sigma=1.0, degree=2)
-    gkernel = _make_kernel(c, normalize=True, ktype="gaussian", sigma=1.0, degree=2)
-    pkernel = _make_kernel(c, normalize=True, ktype="poly", sigma=1.0, degree=2)
-    assert lkernel.shape==gkernel.shape==pkernel.shape == (3,3)
+    lkernel = make_kernel(c, ktype="linear", constant=10.0)
+    gkernel = make_kernel(c, ktype="gaussian", sigma=1.0, degree=2.0)
+    pkernel = make_kernel(c, ktype="poly", sigma=1.0, degree=2.0)
+    assert lkernel.shape==gkernel.shape==pkernel.shape == (10,10)
     
 # Test error handling
 def test_bad_ktype():
@@ -144,21 +121,6 @@ def test_int_sigma():
 def test_neg_degree():
     with pytest.raises(ValueError):
         kcca_g = KCCA(ktype ="poly", reg = 0.001, n_components = 1, degree =-1)
-
-# Test error handling
-def test_float_degree():
-    with pytest.raises(ValueError):
-        kcca_h = KCCA(ktype ="poly", reg = 0.001, n_components = 1, degree =1.0)
-
-# Test error handling
-def test_neg_cutoff():
-    with pytest.raises(ValueError):
-        kcca_i = KCCA(ktype ="poly", reg = 0.001, n_components = 1, cutoff= -1)
-
-# Test error handling
-def test_inf_cutoff():
-    with pytest.raises(ValueError):
-        kcca_j = KCCA(ktype ="poly", reg = 0.001, n_components = 1, cutoff= 1)
         
 # Test if error when transform before fit
 def test_no_weights():
