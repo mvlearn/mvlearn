8fd692f6f159ec0698dd310b39a2eb859207baf7
Gavin <gavinmischler@gmail.com>
2020-03-10 17:51:28 -0400

    working on tests for dcca

--- tests/test_dcca.py
+++ tests/test_dcca.py
@@ -2,19 +2,6 @@ import pytest
 import numpy as np
 from mvlearn.embed.dcca import DCCA
 
-
-random_seed = 10
-N = 600
-input_size1, input_size2 = 100, 100
-sin_transform = []
-np.random.seed(random_seed)
-sin_transform.append(20*np.random.normal(N,input_size1))
-sin_transform.append(np.sin(sin_transform[0]))
-layer_sizes1 = [1024, 50]
-layer_sizes2 = [1024, 50]
-dcca_50_2 = DCCA(input_size1, input_size2, n_components=2,
-                 layer_sizes1=[1024, 50], layer_sizes2=[1024, 50])
-
 @pytest.fixture(scope='module')
 def data():
     random_seed = 10
@@ -28,30 +15,35 @@ def data():
     layer_sizes2 = [1024, 50]
     dcca_50_2 = DCCA(input_size1, input_size2, n_components=2,
                      layer_sizes1=[1024, 50], layer_sizes2=[1024, 50])
+    dcca_print = DCCA(input_size1, input_size2, n_components=2,
+                     layer_sizes1=[1024, 50], layer_sizes2=[1024, 50],
+                     print_train_log_info=True)
     dcca_80_2 = DCCA(input_size1, input_size2, n_components=2,
                      layer_sizes1=[1024, 80], layer_sizes2=[1024, 80])
 
     return {'N' : N, 'input_size1' : input_size1, 'input_size2' : input_size2,
             'layer_sizes1' : layer_sizes1, 'layer_sizes2' : layer_sizes2,
             'sin_transform' : sin_transform, 'random_seed' : random_seed,
-            'dcca_50_2' : dcca_50_2, 'dcca_80_2' : dcca_80_2}
+            'dcca_50_2' : dcca_50_2, 'dcca_80_2' : dcca_80_2,
+            'dcca_print' : dcca_print}
 
 '''
 EXCEPTION TESTING
 '''
 
-def test_bad_input_size():
+def test_bad_input_size(data):
     # incorrect size
     with pytest.raises(ValueError):
         bad_data1 = []
         bad_data1.append(np.random.rand(50, 10))
-        bad_data1.append(np.random.rand(50, input_size2))
-        dcca_50_2.fit(bad_data1)
+        bad_data1.append(np.random.rand(50, data['input_size2']))
+        #dcca_50_2.fit(bad_data1)
+        data['dcca_50_2'].fit(bad_data1)
     with pytest.raises(ValueError):
         bad_data1 = []
-        bad_data1.append(np.random.rand(50, input_size1))
+        bad_data1.append(np.random.rand(50, data['input_size1']))
         bad_data1.append(np.random.rand(50, 5))
-        dcca_50_2.fit(bad_data1)
+        # dcca_50_2.fit(bad_data1)
         data['dcca_50_2'].fit(bad_data1)
     with pytest.raises(ValueError):
         dcca = DCCA(0, 5, 2, [10, 10], [10, 10])
@@ -88,32 +80,59 @@ def test_bad_layer_sizes():
         layer_sizes1, layer_sizes2 = np.array([10, 10]), [10, 10]
         dcca = DCCA(3, 5, 2, layer_sizes1, layer_sizes2)
 
-def test_bad_epoch_num():
+def test_bad_epoch_num(data):
     with pytest.raises(ValueError):
-        dcca = DCCA(5, 5, 2, layer_sizes1, layer_sizes2, epoch_num=0)
+        dcca = DCCA(5, 5, 2, data['layer_sizes1'], data['layer_sizes2'],
+                    epoch_num=0)
     with pytest.raises(ValueError):
-        dcca = DCCA(5, 5, 2, layer_sizes1, layer_sizes2, epoch_num=91.9)
+        dcca = DCCA(5, 5, 2, data['layer_sizes1'], data['layer_sizes2'],
+                    epoch_num=91.9)
 
-def test_bad_batch_size():
+def test_bad_batch_size(data):
     with pytest.raises(ValueError):
-        dcca = DCCA(5, 5, 2, layer_sizes1, layer_sizes2, batch_size=0)
+        dcca = DCCA(5, 5, 2, data['layer_sizes1'], data['layer_sizes2'],
+                    batch_size=0)
     with pytest.raises(ValueError):
-        dcca = DCCA(5, 5, 2, layer_sizes1, layer_sizes2, batch_size=91.9)
+        dcca = DCCA(5, 5, 2, data['layer_sizes1'], data['layer_sizes2'],
+                    batch_size=91.9)
 
-def test_bad_learning_rate():
+def test_bad_learning_rate(data):
     with pytest.raises(ValueError):
-        dcca = DCCA(5, 5, 2, layer_sizes1, layer_sizes2, learning_rate=0)
+        dcca = DCCA(5, 5, 2, data['layer_sizes1'], data['layer_sizes2'],
+                    learning_rate=0)
     with pytest.raises(ValueError):
-        dcca = DCCA(5, 5, 2, layer_sizes1, layer_sizes2, learning_rate=-2)
+        dcca = DCCA(5, 5, 2, data['layer_sizes1'], data['layer_sizes2'],
+                    learning_rate=-4)
 
-def test_bad_reg_par():
+def test_bad_reg_par(data):
     with pytest.raises(ValueError):
-        dcca = DCCA(5, 5, 2, layer_sizes1, layer_sizes2, reg_par=0)
+        dcca = DCCA(5, 5, 2, data['layer_sizes1'], data['layer_sizes2'],
+                    reg_par=0)
     with pytest.raises(ValueError):
-        dcca = DCCA(5, 5, 2, layer_sizes1, layer_sizes2, reg_par=-2)
+        dcca = DCCA(5, 5, 2, data['layer_sizes1'], data['layer_sizes2'],
+                    reg_par=-4)
 
-def test_bad_reg_par():
+def test_bad_reg_par(data):
     with pytest.raises(ValueError):
-        dcca = DCCA(5, 5, 2, layer_sizes1, layer_sizes2, tolerance=0)
+        dcca = DCCA(5, 5, 2, data['layer_sizes1'], data['layer_sizes2'],
+                    tolerance=0)
     with pytest.raises(ValueError):
-        dcca = DCCA(5, 5, 2, layer_sizes1, layer_sizes2, tolerance=-2)
+        dcca = DCCA(5, 5, 2, data['layer_sizes1'], data['layer_sizes2'],
+                    tolerance=-4)
+
+'''
+Performance
+'''
+def test_sin_transform_performance(data):
+    outputs = data['dcca_50_2'].fit_transform(sin_transform)
+    corr = np.correlate(outputs[0][:,0], outputs[1][:,0] /
+            (outputs[0].shape[0]))
+    assert (corr < 1) and (corr > 0.98)
+
+def test_sin_transform_print_train_log(data):
+    outputs = data['dcca_print'].fit_transform(sin_transform)
+    corr = np.correlate(outputs[0][:,0], outputs[1][:,0] /
+            (outputs[0].shape[0]))
+    assert (corr < 1) and (corr > 0.98)
+
+
