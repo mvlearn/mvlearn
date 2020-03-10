2458e7502af9b7a1eb04c84e3b436e504ce935a9
alex <alexc3071@gmail.com>
2020-03-10 06:36:03 -0400

    Removed extraneous files

--- mvlearn/cluster/__init__.py
+++ mvlearn/cluster/__init__.py
@@ -2,4 +2,5 @@ from .mv_spectral import MultiviewSpectralClustering
 from .mv_kmeans import MultiviewKMeans
 from .mv_spherical_kmeans import MultiviewSphericalKMeans
 
-__all__ = ["MultiviewSpectralClustering", "MultiviewKMeans", "MultiviewSphericalKMeans"]
+__all__ = ["MultiviewSpectralClustering", "MultiviewKMeans",
+           "MultiviewSphericalKMeans"]
