c6aa407a99d971c902384e637c16b80e07cd0d4f
alex <alexc3071@gmail.com>
2020-04-01 06:03:37 -0700

    Implemented multiview coregularized spectral clustering with validation

--- mvlearn/cluster/__init__.py
+++ mvlearn/cluster/__init__.py
@@ -1,6 +1,7 @@
 from .mv_spectral import MultiviewSpectralClustering
 from .mv_kmeans import MultiviewKMeans
 from .mv_spherical_kmeans import MultiviewSphericalKMeans
+from .mv_coreg_spectral import MultiviewCoRegSpectralClustering
 
 __all__ = ["MultiviewSpectralClustering", "MultiviewKMeans",
-           "MultiviewSphericalKMeans"]
+           "MultiviewSphericalKMeans", "MultiviewCoRegSpectralClustering"]
