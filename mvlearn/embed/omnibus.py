f335457ccf72e2a12a7ab2f127277014edeb4c0c
Gavin Mischler <23305546+gmischl1@users.noreply.github.com>
2020-04-06 22:43:45 -0400

    Docstring Examples and import updates (#109)
    * added Examples section to every class and updated import mvlearn. statements everywhere to stop at correct module, rather than continuing to file name
    
    * fixed typo in mvmds documentation
    
    * removed accidentally added files
    
    * improved rsm, embed, cotrain, and datasets examples, and fixed datasets.rst file
    
    * added mvmds description and linked references for all classes
    
    * fixed style issue with mvmds
    
    * added clustering reference to docs and use-case
    
    * add single view case for ctclassifier docs
    
    * added MVMDS vs PCA docstring
    
    * updated tutorial mvmds
    
    * copy commit
    
    * fixed whitespace
    
    * recommit
    
    * docstrings passing

--- mvlearn/embed/omnibus.py
+++ mvlearn/embed/omnibus.py
@@ -19,9 +19,8 @@ class Omnibus(BaseEmbed):
     """
     Omnibus computes the pairwise distances for each view. Each
     of these matrices is a n x n dissimilarity matrix where n is the number
-    of rows in each view. Omnibus embedding
-    is then performed over the dissimilarity matrices and the
-    computed embeddings are returned.
+    of rows in each view. Omnibus embedding [#1Omni]_ is then performed
+    over the dissimilarity matrices and the computed embeddings are returned.
 
     Parameters
     ----------
@@ -53,10 +52,28 @@ class Omnibus(BaseEmbed):
         per view. If fit() has not been called, embeddings_ is set to
         None.
 
+    Notes
+    -----
+    From an implementation perspective, omnibus embedding is performed
+    using the GrasPy package's implementation graspy.embed.OmnibusEmbed
+    for dissimilarity matrices.
+
     References
     ----------
-    .. [#1] https://graspy.neurodata.io/tutorials/embedding/omnibus
+    .. [#1Omni] https://graspy.neurodata.io/tutorials/embedding/omnibus
 
+    Examples
+    --------
+    >>> from mvlearn.embed import omnibus
+    >>> import numpy as np
+    >>> # Create 2 random data views with feature sizes 50 and 100
+    >>> view1 = np.random.rand(1000, 50)
+    >>> view2 = np.random.rand(1000, 100)
+    >>> embedder = omnibus.Omnibus(n_components=3)
+    >>> embeddings = embedder.fit_transform([view1, view2])
+    >>> view1_hat, view2_hat = embeddings
+    >>> print(view1_hat.shape, view2_hat.shape)
+    (1000, 3) (1000, 3)
     """
 
     def __init__(self, n_components=2, distance_metric="euclidean",
