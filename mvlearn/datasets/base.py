882bc0485290c23c7292adf45070676afd0626ae
Gavin Mischler <23305546+gmischl1@users.noreply.github.com>
2020-04-06 23:03:55 -0400

    Added shuffle option to UCI dataloader (#112)
    * added shuffle option to dataloader with tests
    
    * switched default shuffle to False but still will shuffle so not ordered
    
    * default None random_state easy

--- mvlearn/datasets/base.py
+++ mvlearn/datasets/base.py
@@ -2,8 +2,9 @@ from os.path import dirname, join
 import numpy as np
 
 
-def load_UCImultifeature(select_labeled="all"):
-    """
+def load_UCImultifeature(select_labeled="all", shuffle=False,
+                         random_state=None):
+    r"""
     Load the UCI multiple features dataset [#1Data]_, taken from
     https://archive.ics.uci.edu/ml/datasets/Multiple+Features This data set
     consists of 6 views of handwritten digit images, with classes 0-9. The
@@ -25,9 +26,17 @@ def load_UCImultifeature(select_labeled="all"):
         specified, all examples in the dataset are returned. Repeated labels
         are ignored.
 
+    shuffle : bool, default=False
+        If ``True``, returns each array with its rows and corresponding
+        labels shuffled randomly according to ``random_state``.
+
+    random_state : int, default=None
+        Determines the order data is shuffled if ``shuffle=True``. Used so
+        that data loaded is reproducible but shuffled.
+
     Returns
     -------
-    data : list of np.ndarray, each of size (2000,n_features)
+    data : list of np.ndarray, each of size (200*num_classes, n_features)
         List of length 6 with each element being the data for one of the
         views.
 
@@ -64,6 +73,9 @@ def load_UCImultifeature(select_labeled="all"):
     if select_labeled == "all":
         select_labeled = range(10)
 
+    if not shuffle:
+        random_state = 1
+
     select_labeled = list(set(select_labeled))
 
     if len(select_labeled) < 1 or len(select_labeled) > 10:
@@ -94,6 +106,12 @@ def load_UCImultifeature(select_labeled="all"):
             indices = np.nonzero(labels == label)
             datatemp[j * 200: (j+1) * 200, :] = data[i][indices, :]
             selected_labels[j*200:(j+1)*200] = labels[indices]
+
+        np.random.seed(random_state)
+        np.random.shuffle(datatemp)
         selected_data.append(datatemp)
+
+    np.random.seed(random_state)
+    np.random.shuffle(selected_labels)
 
     return selected_data, selected_labels
