eab1ee17f8770fce3e50d7af346493b62426958c
rflperry <rflperry@gmail.com>
2020-04-19 19:07:19 -0400

    import fix

--- mvlearn/embed/__init__.py
+++ mvlearn/embed/__init__.py
@@ -1,17 +1,23 @@
 from .gcca import GCCA
 from .omnibus import Omnibus
 from .mvmds import MVMDS
-from .splitae import SplitAE
+try:
+    from .splitae import SplitAE
+    from .dcca import DCCA, linear_cca, cca_loss, MlpNet, DeepPairedNetworks
+except ModuleNotFoundError as er:
+    torch=False
 from .kcca import KCCA
-from .dcca import DCCA, linear_cca, cca_loss, MlpNet, DeepPairedNetworks
 from .utils import select_dimension
 
 __all__ = [
-    "GCCA",
-    "Omnibus",
-    "MVMDS",
-    "SplitAE",
-    "KCCA",
-    "DCCA",
-    "select_dimension",
-]
+        "GCCA",
+        "Omnibus",
+        "MVMDS",
+        "KCCA",
+        "select_dimension",
+    ]
+if 'torch' not in locals():
+    __all__ += [
+        "SplitAE",
+        "DCCA",
+    ]
