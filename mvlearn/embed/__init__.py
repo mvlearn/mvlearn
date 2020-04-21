ffa724a8c4281ca364f3d0efaecad3141782f1bb
rflperry <rflperry@gmail.com>
2020-04-20 23:15:03 -0400

    error on direct import

--- mvlearn/embed/__init__.py
+++ mvlearn/embed/__init__.py
@@ -1,11 +1,7 @@
+import sys
 from .gcca import GCCA
 from .omnibus import Omnibus
 from .mvmds import MVMDS
-try:
-    from .splitae import SplitAE
-    from .dcca import DCCA, linear_cca, cca_loss, MlpNet, DeepPairedNetworks
-except ModuleNotFoundError as er:
-    torch=False
 from .kcca import KCCA
 from .utils import select_dimension
 
@@ -16,8 +12,11 @@ __all__ = [
         "KCCA",
         "select_dimension",
     ]
-if 'torch' not in locals():
-    __all__ += [
-        "SplitAE",
-        "DCCA",
-    ]
+if 'torch' in sys.modules:
+    from .dcca import DCCA, linear_cca, cca_loss, MlpNet, DeepPairedNetworks
+    __all__ += ["DCCA"]
+if 'torch' in sys.modules and 'torchvision' in sys.modules:
+    from .splitae import SplitAE
+    __all__ += ["SplitAE"]
+
+    
