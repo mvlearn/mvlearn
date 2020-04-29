2e053a9ab5538e44ec17245271df8ec944139e8a
rflperry <rflperry@gmail.com>
2020-04-29 12:04:49 -0400

    update init

--- mvlearn/embed/__init__.py
+++ mvlearn/embed/__init__.py
@@ -4,18 +4,8 @@ from .omnibus import Omnibus
 from .mvmds import MVMDS
 from .kcca import KCCA
 from .utils import select_dimension
-try:
-    import torch
-    from torch.utils.data import Dataset, DataLoader
-    from torchvision import datasets
-    import torchvision
-    import torch.nn as nn
-    from torch.utils.data import BatchSampler, SequentialSampler, RandomSampler
-
-    from .dcca import DCCA, linear_cca, cca_loss, MlpNet, DeepPairedNetworks
-    from .splitae import SplitAE
-except ModuleNotFoundError:
-    pass
+from .dcca import DCCA
+from .splitae import SplitAE
 
 __all__ = [
         "GCCA",
