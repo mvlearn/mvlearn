5e2811ce8c09d1cb4d170f0347563d6f4db1ce17
Pierre Ablin <pierreablin@gmail.com>
2020-09-09 12:20:29 +0200

    first draft

--- mvlearn/decomposition/grouppca.py
+++ mvlearn/decomposition/grouppca.py
@@ -19,33 +19,47 @@ class GroupPCA(BaseEstimator):
     def fit_transform(self, Xs, y=None):
         Xs = check_Xs(Xs)
         n_features = [X.shape[1] for X in Xs]
+        self.n_subjects_ = len(Xs)
+        self.n_features_ = n_features
+        self.n_samples_ = X.shape[0]
         self.individual_projection_ = self.n_individuals is not None
         if self.n_components is None:
             self.n_components_ = min(n_features)
             if self.individual_projection_:
-                self.n_components_ = min(self.n_components_, self.n_individuals)
+                self.n_components_ = min(self.n_components_,
+                                         self.n_individuals)
         else:
             self.n_components_ = self.n_components
 
         if self.individual_projection_:
             self.individual_components_ = []
             self.individual_explained_variance_ = []
-            self.individual_means_ = []
+            self.individual_mean_ = []
             for i, X in enumerate(Xs):
                 pca = PCA(self.n_individuals, whiten=self.whiten)
                 Xs[i] = pca.fit_transform(X)
                 self.individual_components_.append(pca.components_)
-                self.individual_explained_variance_.append(pca.explained_variance_)
-                self.individual_means_.append(pca.mean_)
+                self.individual_explained_variance_.append(
+                    pca.explained_variance_
+                    )
+                self.individual_mean_.append(pca.mean_)
         X_stack = np.hstack(Xs)
-        output = PCA(self.n_components_, whiten=self.whiten).fit_transform(X_stack)
+        pca = PCA(self.n_components_, whiten=self.whiten)
+        output = pca.fit_transform(X_stack)
         self.components_ = pca.components_
         self.explained_variance_ = pca.explained_variance_
-        self.mean_ = pca.mean_
+        self.mean_ = [np.mean(X, axis=0) for X in Xs]
         if self.individual_output:
             return output
         else:
-            return
+            self.group_components_ = []
+            outputs = []
+            for X, mean in zip(Xs, self.mean_):
+                X = X - mean
+                components = np.dot(X, output).T
+                self.group_components_.append(components)
+                outputs.append(np.dot(X, components.T))
+            return outputs
 
     def fit(self, Xs, y=None):
         self.fit_transform(Xs, y)
@@ -53,4 +67,24 @@ class GroupPCA(BaseEstimator):
 
     def transform(self, Xs):
         if self.individual_projection_:
-            return
+            for i, (X, mean, components_, explained_variance_) in (
+                enumerate(zip(Xs, self.individual_mean_,
+                              self.individual_components_,
+                              self.individual_explained_variance_s))
+                                                                   ):
+                X = X - mean
+                X_transformed = np.dot(X, components_.T)
+                if self.whiten:
+                    X_transformed /= np.sqrt(explained_variance_)
+                Xs[i] = X_transformed
+        Xs = [X - mean for X, mean in zip(Xs, self.mean_)]
+        if self.individual_output:
+            X_stack = np.hstack(Xs)
+            X_transformed = np.dot(X_stack, self.components_.T)
+            if self.whiten:
+                X_transformed /= np.sqrt(self.explained_variance_)
+        else:
+            X_transformed = []
+            for X, group_components_ in zip(X, self.group_components_):
+                X_transformed.append(np.dot(X, group_components_.T))
+        return X_transformed
