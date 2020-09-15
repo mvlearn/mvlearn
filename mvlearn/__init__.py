9b9b2fc2b7e32268fc8cc16277bbce9df9f5f566
Pierre Ablin <pierreablin@gmail.com>
2020-09-15 12:07:26 +0200

    fix flake8

--- mvlearn/__init__.py
+++ mvlearn/__init__.py
@@ -5,7 +5,8 @@ import mvlearn.semi_supervised
 import mvlearn.datasets
 import mvlearn.utils
 import mvlearn.decomposition
-import mvlearn.plotting  # noqa
 import mvlearn.merge
+import mvlearn.plotting  # noqa
+
 
 __version__ = "0.3.0"
