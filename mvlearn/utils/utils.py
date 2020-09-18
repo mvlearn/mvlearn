d76a073b5524fcf977de5d051502148444f3a011
Pierre Ablin <pierreablin@gmail.com>
2020-09-18 16:51:52 +0200

    init

--- mvlearn/utils/utils.py
+++ mvlearn/utils/utils.py
@@ -16,7 +16,13 @@ import numpy as np
 from sklearn.utils import check_X_y, check_array
 
 
-def check_Xs(Xs, multiview=False, enforce_views=None, copy=False):
+def check_Xs(
+    Xs,
+    multiview=False,
+    enforce_views=None,
+    copy=False,
+    return_dimensions=False,
+):
     r"""
     Checks Xs and ensures it to be a list of 2D matrices.
 
@@ -38,10 +44,28 @@ def check_Xs(Xs, multiview=False, enforce_views=None, copy=False):
         the input.
         If False, the returned Xs is a view of the input Xs,
         and operations on the output will change the input.
+
+    return_dimensions : boolean, (default=False)
+        If True, the function also returns the dimensions of the multiview
+        dataset. The dimensions are n_views, n_samples, n_features where
+        n_samples and n_views are respectively the number of views and the
+        number of samples, and n_features is a list of length n_views
+        containing the number of features of each view.
+
     Returns
     -------
     Xs_converted : object
         The converted and validated Xs (list of data arrays).
+
+    n_views : int, returned only if return_dimensions is True
+        The number of views in the dataset.
+
+    n_samples : int, returned only if return_dimensions is True
+        The number of samples in the dataset.
+
+    n_views : list, returned only if return_dimensions is True
+        List of lenght n_views containing the number of features for each
+        view.
     """
     if not isinstance(Xs, list):
         if not isinstance(Xs, np.ndarray):
@@ -53,17 +77,19 @@ def check_Xs(Xs, multiview=False, enforce_views=None, copy=False):
         else:
             Xs = list(Xs)
 
-    if len(Xs) == 0:
+    n_views = len(Xs)
+    if n_views == 0:
         msg = "Length of input list must be greater than 0"
         raise ValueError(msg)
 
     if multiview:
-        if len(Xs) == 1:
+        if n_views == 1:
             msg = "Must provide at least two data matrices"
             raise ValueError(msg)
-        if enforce_views is not None and len(Xs) != enforce_views:
-            msg = "Wrong number of views. Expected {} " \
-                " but found {}".format(enforce_views, len(Xs))
+        if enforce_views is not None and n_views != enforce_views:
+            msg = "Wrong number of views. Expected {} " " but found {}".format(
+                enforce_views, len(Xs)
+            )
             raise ValueError(msg)
 
     Xs_converted = [check_array(X, allow_nd=False, copy=copy) for X in Xs]
@@ -72,17 +98,24 @@ def check_Xs(Xs, multiview=False, enforce_views=None, copy=False):
         msg = "All views must have the same number of samples"
         raise ValueError(msg)
 
-    return Xs_converted
+    if not return_dimensions:
+        return Xs_converted
+    else:
+        n_samples = Xs[0].shape[0]
+        n_features = [X.shape[1] for X in Xs]
+        return Xs_converted, n_views, n_samples, n_features
 
 
-def check_Xs_y(Xs, y, multiview=False, enforce_views=None):
+def check_Xs_y(
+    Xs, y, multiview=False, enforce_views=None, return_dimensions=False
+):
     r"""
     Checks Xs and y for consistent length. Xs is set to be of dimension 3.
 
     Parameters
     ----------
     Xs : nd-array, list
-        Input data. F
+        Input data.
 
     y : nd-array, list
         Labels.
@@ -94,6 +127,13 @@ def check_Xs_y(Xs, y, multiview=False, enforce_views=None):
         If provided, ensures this number of views in Xs. Otherwise not
         checked.
 
+    return_dimensions : boolean, (default=False)
+        If True, the function also returns the dimensions of the multiview
+        dataset. The dimensions are n_views, n_samples, n_features where
+        n_samples and n_views are respectively the number of views and the
+        number of samples, and n_features is a list of length n_views
+        containing the number of features of each view.
+
     Returns
     -------
     Xs_converted : object
@@ -101,23 +141,45 @@ def check_Xs_y(Xs, y, multiview=False, enforce_views=None):
 
     y_converted : object
         The converted and validated y.
+
+    n_views : int, returned only if return_dimensions is True
+        The number of views in the dataset.
+
+    n_samples : int, returned only if return_dimensions is True
+        The number of samples in the dataset.
+
+    n_views : list, returned only if return_dimensions is True
+        List of lenght n_views containing the number of features for each
+        view.
     """
-    Xs_converted = check_Xs(Xs, multiview=multiview,
-                            enforce_views=enforce_views)
+    if return_dimensions:
+        Xs_converted, n_views, n_samples, n_features = check_Xs(
+            Xs,
+            multiview=multiview,
+            enforce_views=enforce_views,
+            return_dimensions=True,
+        )
+    else:
+        Xs_converted = check_Xs(
+            Xs, multiview=multiview, enforce_views=enforce_views
+        )
     _, y_converted = check_X_y(Xs_converted[0], y, allow_nd=False)
 
-    return Xs_converted, y_converted
+    if not return_dimensions:
+        return Xs_converted, y_converted
+    else:
+        return Xs_converted, y_converted, n_views, n_samples, n_features
 
 
 def check_Xs_y_nan_allowed(
-        Xs,
-        y,
-        multiview=False,
-        enforce_views=None,
-        num_classes=None,
-        max_classes=None,
-        min_classes=None
-        ):
+    Xs,
+    y,
+    multiview=False,
+    enforce_views=None,
+    num_classes=None,
+    max_classes=None,
+    min_classes=None,
+):
     r"""
     Checks Xs and y for consistent length. Xs is set to be of dimension 3.
     The labels (y) are allowed to be np.nan.
@@ -151,15 +213,18 @@ def check_Xs_y_nan_allowed(
         The converted and validated y.
     """
 
-    Xs_converted = check_Xs(Xs,
-                            multiview=multiview,
-                            enforce_views=enforce_views)
+    Xs_converted = check_Xs(
+        Xs, multiview=multiview, enforce_views=enforce_views
+    )
 
     y_converted = np.array(y)
     if len(y_converted) != Xs_converted[0].shape[0]:
-        msg = "Incompatible label length {} for "\
+        msg = (
+            "Incompatible label length {} for "
             " data with {} samples".format(
-                len(y_converted), Xs_converted[0].shape[0])
+                len(y_converted), Xs_converted[0].shape[0]
+            )
+        )
         raise ValueError(msg)
 
     if num_classes is not None:
@@ -167,14 +232,22 @@ def check_Xs_y_nan_allowed(
         classes = list(set(y[~np.isnan(y)]))
         n_classes = len(classes)
         if n_classes != num_classes:
-            raise ValueError("Wrong number of class labels. Expected {},\
-             found {}".format(num_classes, n_classes))
+            raise ValueError(
+                "Wrong number of class labels. Expected {},\
+             found {}".format(
+                    num_classes, n_classes
+                )
+            )
     if max_classes is not None:
         # if not exactly correct number of class labels, raise error
         classes = list(set(y[~np.isnan(y)]))
         n_classes = len(classes)
         if n_classes > max_classes:
-            raise ValueError("Wrong number of class labels. Expected no\
-             more than {}, found {}".format(num_classes, n_classes))
+            raise ValueError(
+                "Wrong number of class labels. Expected no\
+             more than {}, found {}".format(
+                    num_classes, n_classes
+                )
+            )
 
     return Xs_converted, y_converted
