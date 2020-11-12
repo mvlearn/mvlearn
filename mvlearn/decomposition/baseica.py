c394d6891fe543c84f104add66452f4987e3493b
hugorichard <hugorichard@users.noreply.github.com>
2020-11-12 16:55:15 +0100

    Update mvlearn/decomposition/baseica.py
    Co-authored-by: Alexandre Gramfort <alexandre.gramfort@m4x.org>

--- mvlearn/decomposition/baseica.py
+++ mvlearn/decomposition/baseica.py
@@ -1,6 +1,6 @@
 # MIT License
 
-# Copyright (c) [2017] [Pierre Ablin and Hugo Richard]
+# Copyright (c) [2020] [Pierre Ablin and Hugo Richard]
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
