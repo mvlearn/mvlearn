36cc8ef7a5b07cc5b120c7022df14412ed84f3cc
Ronan Perry <rflperry@MacBook-Pro-2.local>
2020-11-13 10:25:21 -0500

    example update

--- examples/pipeline/plot_multiview_construction.py
+++ examples/pipeline/plot_multiview_construction.py
@@ -20,8 +20,14 @@ the number of features in the singleview data.
 Two subsampling methods are applied. The random subspace method simply selects
 a random subset of the features. The random Gaussian projection method creates
 new features by sampling random multivariate Gaussian vectors used to project
-the original features. The latter method can potentially help in nonlinear
+the original features. The latter method can potentially help in complicated
 settings where combinations of features better capture informative relations.
+
+It is clear that subsampling features in this setting leads to improved
+out-of-sample accuracy, most likely as it reduces overfitting to the large
+number of raw features. This is confirmed as the accuracy seems to peak
+around when the number of features is equal to the number of samples, at which
+point overfitting becomes possible.
 """
 
 # Author: Ronan Perry
@@ -32,63 +38,59 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.pipeline import make_pipeline
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import cross_val_score, ShuffleSplit
 from sklearn.datasets import fetch_olivetti_faces
 from mvlearn.compose import RandomSubspaceMethod, RandomGaussianProjection, \
     ViewClassifier
 
-
 # Load the singleview Olivevetti faces dataset from sklearn
 X, y = fetch_olivetti_faces(return_X_y=True)
-
-rsm_scores = []
-rgp_scores = []
-clf_scores = []
 
 # The data has 4096 features. The following subspace dimensions are used
 dims = [16, 64, 256, 1024]
 
-skf = StratifiedKFold(n_splits=5, random_state=0, shuffle=True)
-skf.get_n_splits(X, y)
+# We are interested in when the low sample size, high dimensionality setting
+train_size = 0.2
+rsm_scores = []
+rgp_scores = []
 
-for i, (test_index, train_index) in enumerate(skf.split(X, y)):
-    X_train, X_test = X[train_index], X[test_index]
-    y_train, y_test = y[train_index], y[test_index]
-    rsm_scores.append([])
-    rgp_scores.append([])
-    for dim in dims:
-        n_views = int(X.shape[1] / dim)
+# Initialze cross validation
+splitter = ShuffleSplit(n_splits=5, train_size=train_size, random_state=0)
 
-        rsm_clf = make_pipeline(
+# Compute singleview score, using all dimensions
+singleview_clf = make_pipeline(StandardScaler(), LinearDiscriminantAnalysis())
+singleview_scores = cross_val_score(singleview_clf, X, y, cv=splitter)
+
+# For each dimension, we compute scores for a multiview classifier
+for dim in dims:
+    n_views = int(X.shape[1] / dim)
+
+    rsm_clf = make_pipeline(
             StandardScaler(),
             RandomSubspaceMethod(n_views=n_views, subspace_dim=dim),
             ViewClassifier(LinearDiscriminantAnalysis())
         )
-        rsm_scores[i].append(
-            rsm_clf.fit(X_train, y_train).score(X_test, y_test))
+    rsm_scores.append(cross_val_score(rsm_clf, X, y, cv=splitter))
 
-        rgp_clf = make_pipeline(
-            StandardScaler(),
-            RandomGaussianProjection(n_views=n_views, n_components=dim),
-            ViewClassifier(LinearDiscriminantAnalysis())
-        )
-        rgp_scores[i].append(
-            rgp_clf.fit(X_train, y_train).score(X_test, y_test))
+    rgp_clf = make_pipeline(
+        StandardScaler(),
+        RandomGaussianProjection(n_views=n_views, n_components=dim),
+        ViewClassifier(LinearDiscriminantAnalysis())
+    )
+    rgp_scores.append(cross_val_score(rgp_clf, X, y, cv=splitter))
 
-    clf = make_pipeline(StandardScaler(), LinearDiscriminantAnalysis())
-    clf_scores.append(clf.fit(X_train, y_train).score(X_test, y_test))
-
+# The results are plotted
 fig, ax = plt.subplots()
-ax.axhline(
-    np.mean(clf_scores), ls='--', c='grey', label='LDA singleview score')
-ax.axvline(
-    X_train.shape[0], ls=':', c='grey', label='Number of training samples')
+ax.axvline(X.shape[0] * train_size, ls=':', c='grey',
+           label='Number of training samples')
+ax.axhline(np.mean(singleview_scores), ls='--', c='grey',
+           label='LDA singleview score')
 ax.errorbar(
-    dims, np.mean(rsm_scores, axis=0),
-    yerr=np.std(rsm_scores, axis=0), label='LDA o Random Subspace')
+    dims, np.mean(rsm_scores, axis=1),
+    yerr=np.std(rsm_scores, axis=1), label='LDA o Random Subspace')
 ax.errorbar(
-    dims, np.mean(rgp_scores, axis=0),
-    yerr=np.std(rgp_scores, axis=0), label='LDA o Random Gaussian Projection')
+    dims, np.mean(rgp_scores, axis=1),
+    yerr=np.std(rgp_scores, axis=1), label='LDA o Random Gaussian Projection')
 ax.set_xlabel('Number of subsampled dimensions')
 ax.set_ylabel('Score')
 plt.title('Classification accuracy using constructed multiview data')
