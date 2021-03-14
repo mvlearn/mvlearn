c4813b81bcff42cde368117c3a51d126044f9f82
Ronan Perry <rflperry@MacBook-Pro-2.local>
2021-03-14 17:47:51 -0400

    base -> uci

--- mvlearn/datasets/__init__.py
+++ mvlearn/datasets/__init__.py
@@ -1,9 +1,11 @@
 from .uci_multifeature import load_UCImultifeature
 from .gaussian_mixture import make_gaussian_mixture
 from .factor_model import sample_joint_factor_model
+from .nutrimouse import load_nutrimouse
 
 __all__ = [
     "load_UCImultifeature",
     "make_gaussian_mixture",
-    "sample_joint_factor_model"
+    "sample_joint_factor_model",
+    "load_nutrimouse",
     ]
